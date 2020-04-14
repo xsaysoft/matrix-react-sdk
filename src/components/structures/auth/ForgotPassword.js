5bbbd8c18e560143467c6676a91c075e4b18c03e
Godstime <lewisagho47@gmail.com>
2020-04-14 19:31:59 +0100

    Make create account text clickabe, Changes Matrix to Onescrin in ForgotPassword.js

--- src/components/structures/auth/ForgotPassword.js
+++ src/components/structures/auth/ForgotPassword.js
@@ -242,13 +242,13 @@ export default createReactClass({
             );
         }
 
-        let yourMatrixAccountText = _t('Your Matrix account on %(serverName)s', {
+        let yourMatrixAccountText = _t('Your Onescrein account on %(serverName)s', {
             serverName: this.props.serverConfig.hsName,
         });
         if (this.props.serverConfig.hsNameIsDifferent) {
             const TextWithTooltip = sdk.getComponent("elements.TextWithTooltip");
 
-            yourMatrixAccountText = _t('Your Matrix account on <underlinedServerName />', {}, {
+            yourMatrixAccountText = _t('Your Onescrin account on <underlinedServerName />', {}, {
                 'underlinedServerName': () => {
                     return <TextWithTooltip
                         class="mx_Login_underlinedServerName"
