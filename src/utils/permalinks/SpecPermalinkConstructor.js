1e667840dbadee4b74c5c6950ee71022f3d9070a
mhizsean <adeyiesther01@gmail.com>
2020-04-15 13:37:27 +0100

    assigned a new host

--- src/utils/permalinks/SpecPermalinkConstructor.js
+++ src/utils/permalinks/SpecPermalinkConstructor.js
@@ -16,7 +16,7 @@ limitations under the License.
 
 import PermalinkConstructor, {PermalinkParts} from "./PermalinkConstructor";
 
-export const host = "matrix.to";
+export const host = "onescrin";
 export const baseUrl = `https://${host}`;
 
 /**
